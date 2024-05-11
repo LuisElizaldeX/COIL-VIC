ad0b753e200f5eab52a8b146a711da0e176c65a5
LuisElizaldeX <king_mbea@hotmail.com>
2024-05-11 17:06:38 -0600

    Creacion de carpetas de vistas y controladores

--- src/coilvic/COILVIC.java
+++ src/coilvic/COILVIC.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML.java to edit this template
- */
+
 package coilvic;
 
 import javafx.application.Application;
