5ee22f995f2f0c7fdfc8938d6ef2031c73d3bcbd
LuisElizaldeX <king_mbea@hotmail.com>
2024-05-13 22:14:06 -0600

    Configuracion de "FXMLPaginaPrincipalCoordinador"
    Se modificó el diseño de la página principal coordinador, apegado a los prototipos realizados.

--- src/coilvic/controladores/FXMLIniciarSesionController.java
+++ src/coilvic/controladores/FXMLIniciarSesionController.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
- */
 package coilvic.controladores;
 
 import java.net.URL;
@@ -10,25 +6,28 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
-/**
- *
- * @author king_
- */
 public class FXMLIniciarSesionController implements Initializable {
-    
+
     @FXML
     private Label label;
-    
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+    private PasswordField pfContraseña;
+    @FXML
+    private TextField tfNombreUsuario;
+
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void clicBtnIniciarSesion(ActionEvent event) {
+    }
     
 }
